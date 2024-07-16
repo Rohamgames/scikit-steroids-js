f69a0992ff64d74c900a6189764b78e0c84a6466
Roham <Rohamgames@gmail.com>
2024-07-16 10:23:50 +0000

    -LESS CONDITIONS BETTER CODE

--- yes/neighbors/KNeighborsClassifier.js
+++ yes/neighbors/KNeighborsClassifier.js
@@ -17,43 +17,62 @@ export default class KNeighborsClassifier {
     }
 
     predict(X) {
-        const distancesKernel = this.gpu.createKernel(function(x, X_train, metric, p) {
+        const minkowskiKernel = this.gpu.createKernel(function(x, X_train, p) {
             let distance = 0;
             for (let i = 0; i < this.constants.dim; i++) {
-                if (metric === 0) {  // minkowski
-                    distance += Math.pow(Math.abs(x[i] - X_train[this.thread.x][i]), p);
-                } else if (metric === 1) {  // hamming
-                    distance += (x[i] !== X_train[this.thread.x][i] ? 1 : 0);
-                } else if (metric === 2) {  // cosine
-                    distance += x[i] * X_train[this.thread.x][i];
-                }
+                distance += Math.pow(Math.abs(x[i] - X_train[this.thread.x][i]), p);
             }
-            if (metric === 0) {  // minkowski
-                return Math.pow(distance, 1 / p);
-            } else if (metric === 1) {  // hamming
-                return distance / this.constants.dim;
-            } else if (metric === 2) {  // cosine
-                let magnitudeX = 0, magnitudeTrain = 0;
-                for (let i = 0; i < this.constants.dim; i++) {
-                    magnitudeX += x[i] * x[i];
-                    magnitudeTrain += X_train[this.thread.x][i] * X_train[this.thread.x][i];
-                }
-                return 1 - (distance / (Math.sqrt(magnitudeX) * Math.sqrt(magnitudeTrain)));
-            }
+            return Math.pow(distance, 1 / p);
         }, {
             constants: { dim: this.X_train[0].length },
             output: [this.X_train.length]
         });
 
+        const hammingKernel = this.gpu.createKernel(function(x, X_train) {
+            let distance = 0;
+            for (let i = 0; i < this.constants.dim; i++) {
+                distance += (x[i] !== X_train[this.thread.x][i] ? 1 : 0);
+            }
+            return distance / this.constants.dim;
+        }, {
+            constants: { dim: this.X_train[0].length },
+            output: [this.X_train.length]
+        });
+
+        const cosineKernel = this.gpu.createKernel(function(x, X_train) {
+            let dotProduct = 0;
+            let magnitudeX = 0;
+            let magnitudeTrain = 0;
+            for (let i = 0; i < this.constants.dim; i++) {
+                dotProduct += x[i] * X_train[this.thread.x][i];
+                magnitudeX += x[i] * x[i];
+                magnitudeTrain += X_train[this.thread.x][i] * X_train[this.thread.x][i];
+            }
+            return 1 - (dotProduct / (Math.sqrt(magnitudeX) * Math.sqrt(magnitudeTrain)));
+        }, {
+            constants: { dim: this.X_train[0].length },
+            output: [this.X_train.length]
+        });
+
+        const getDistances = (x) => {
+            if (this.metric === 'minkowski') {
+                return minkowskiKernel(x, this.X_train, this.p);
+            } else if (this.metric === 'hamming') {
+                return hammingKernel(x, this.X_train);
+            } else if (this.metric === 'cosine') {
+                return cosineKernel(x, this.X_train);
+            }
+        };
+
         return X.map(x => {
-            const distances = distancesKernel(x, this.X_train, this.metric === 'minkowski' ? 0 : this.metric === 'hamming' ? 1 : 2, this.p);
+            const distances = getDistances(x);
             const distanceArray = Array.from(distances);
             const kNearestNeighbors = distanceArray
                 .map((distance, i) => ({ distance, label: this.y_train[i] }))
                 .sort((a, b) => a.distance - b.distance)
                 .slice(0, this.n_neighbors);
-
             const counts = {};
+
             kNearestNeighbors.forEach(neighbor => {
                 const label = neighbor.label;
                 counts[label] = (counts[label] || 0) + 1;
