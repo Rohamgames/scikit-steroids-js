abdd7e03d74721ff1dd1baf8d89b79c9aec91463
Roham <Rohamgames@gmail.com>
2024-07-18 00:50:43 +0000

    -BETTER LR

--- yes/linearModel/LinearRegression.js
+++ yes/linearModel/LinearRegression.js
@@ -1,27 +1,19 @@
 import * as math from 'mathjs'
 import {GPU} from 'gpu.js'
+
 export default class LinearRegression {
-    constructor() {
+    constructor(mode='cpu') {
         this.weights = null;
-        this.gpu = new GPU();
+        this.gpu = new GPU({ mode });
+        // TODO: implement GPU
     }
 
     fit(X, y) {
-        console.time('transpose');
         const Xt = math.transpose(X);
-        console.timeEnd('transpose');
-        console.time('multiply');
         const XtX = math.multiply(Xt, X);
-        console.timeEnd('multiply');
-        console.time('inv');
         const XtX_inv = math.inv(XtX);
-        console.timeEnd('inv');
-        console.time('m2');
         const XtX_inv_Xt = math.multiply(XtX_inv, Xt);
-        console.timeEnd('m2');
-        console.time('we');
         this.weights = math.multiply(XtX_inv_Xt, y);
-        console.timeEnd('we');
     }
 
     predict(X) {
